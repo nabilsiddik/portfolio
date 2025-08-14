fa61c22979869e7575ca935f93a5db9a25984e92
nabilsiddik <nabilsiddik90@gmail.com>
2025-08-14 18:17:16 +0600

    skills section done

--- src/components/ui/ClassicTab.tsx
+++ src/components/ui/ClassicTab.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Badge } from "@/components/ui/badge"
-import { BackendIcon, BootstrapIcon, CssIcon, FrontEndIcon, HtmlIcon, NextJsIcon, OtherSkillsIcon, ReactIcon, SassIcon, TailwindIcon } from '@/svg/SkillIcons';
+import { Adaptablity, BackendIcon, BootstrapIcon, CollaborationIcon, CommunicationIcon, CssIcon, ExpressIcon, FigmaIcon, FrontEndIcon, HtmlIcon, JavaScriptIcon, MongoIcon, MongooseIcon, NextJsIcon, NodeIcon, OtherSkillsIcon, PostmanIcon, ProjectManagementIcon, ReactIcon, ReduxIcon, RestApiIcon, SassIcon, TailwindIcon, TeamworkIcon, TypeScriptIcon } from '@/svg/SkillIcons';
 
 
 export default function ClassicTab() {
@@ -33,6 +33,10 @@ export default function ClassicTab() {
           name: 'React.js',
         },
         {
+          icon: <ReduxIcon />,
+          name: 'Redux Toolkit',
+        },
+        {
           icon: <NextJsIcon />,
           name: 'Next.js',
         },
@@ -43,6 +47,18 @@ export default function ClassicTab() {
         {
           icon: <SassIcon />,
           name: 'SASS',
+        },
+        {
+          icon: <FigmaIcon />,
+          name: 'Figma',
+        },
+        {
+          icon: <JavaScriptIcon />,
+          name: 'Javascript',
+        },
+        {
+          icon: <TypeScriptIcon />,
+          name: 'Typescript',
         },
       ]
     },
@@ -53,32 +69,42 @@ export default function ClassicTab() {
       color: 'from-purple-500 to-indigo-500',
       content: [
         {
-          icon: <HtmlIcon />,
-          name: 'HTML 5',
+          icon: <NodeIcon />,
+          name: 'Node.js',
         },
         {
-          icon: <CssIcon />,
-          name: 'CSS 3',
+          icon: <ExpressIcon />,
+          name: 'Express.js',
         },
         {
-          icon: <TailwindIcon />,
-          name: 'Tailwind CSS',
+          icon: <MongoIcon />,
+          name: 'MongoDB',
         },
+        ,
         {
-          icon: <ReactIcon />,
-          name: 'React.js',
+          icon: <MongooseIcon />,
+          name: 'Mongoose',
         },
         {
           icon: <NextJsIcon />,
           name: 'Next.js',
         },
+        ,
         {
-          icon: <BootstrapIcon />,
-          name: 'Bootstrap',
+          icon: <PostmanIcon />,
+          name: 'Post Man',
         },
         {
-          icon: <SassIcon />,
-          name: 'SASS',
+          icon: <RestApiIcon />,
+          name: 'REST API',
+        },
+        {
+          icon: <JavaScriptIcon />,
+          name: 'Javascript',
+        },
+        {
+          icon: <TypeScriptIcon />,
+          name: 'Typescript',
         },
       ]
     },
@@ -89,32 +115,24 @@ export default function ClassicTab() {
       color: 'from-blue-500 to-cyan-500',
       content: [
         {
-          icon: <HtmlIcon />,
-          name: 'HTML 5',
+          icon: <ProjectManagementIcon />,
+          name: 'Project Management',
         },
         {
-          icon: <CssIcon />,
-          name: 'CSS 3',
+          icon: <CommunicationIcon />,
+          name: 'Communication',
         },
         {
-          icon: <TailwindIcon />,
-          name: 'Tailwind CSS',
+          icon: <CollaborationIcon />,
+          name: 'Colaboration',
         },
         {
-          icon: <ReactIcon />,
-          name: 'React.js',
+          icon: <TeamworkIcon />,
+          name: 'Team Work',
         },
         {
-          icon: <NextJsIcon />,
-          name: 'Next.js',
-        },
-        {
-          icon: <BootstrapIcon />,
-          name: 'Bootstrap',
-        },
-        {
-          icon: <SassIcon />,
-          name: 'SASS',
+          icon: <Adaptablity />,
+          name: 'Adaptability',
         },
       ]
     },
@@ -158,7 +176,7 @@ export default function ClassicTab() {
                 >
                   {tab.icon}
                 </motion.div>
-                <span className='font-bold tracking-wide text-center text-xl lg:text-2xl mt-3'>
+                <span className='font-bold tracking-wide text-center text-md md:text-lg lg:text-2xl mt-3'>
                   {tab.name}
                 </span>
               </div>
@@ -187,9 +205,9 @@ export default function ClassicTab() {
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -20 }}
             transition={{ duration: 0.3 }}
-            className={`relative overflow-hidden rounded-xl p-6 bg-white dark:bg-gray-800 shadow-lg border-t-4 border-gradient-to-r ${tabs.find((t) => t.id === activeTab)?.color}`}
+            className={`relative overflow-hidden rounded-xl px-8 py-8 bg-white dark:bg-gray-800 shadow-lg border-t-4 border-gradient-to-r ${tabs.find((t) => t.id === activeTab)?.color}`}
           >
-            <div className='absolute top-0 right-0 p-2 flex items-center justify-center'>
+            <div className='absolute top-0 right-0 p-2 lg:p-3 flex items-center justify-center'>
               <motion.div
                 className='text-2xl opacity-50'
                 animate={{
@@ -207,30 +225,32 @@ export default function ClassicTab() {
               </motion.div>
             </div>
 
-            <h3 className='text-2xl font-bold mb-7 text-gray-800 dark:text-white'>
+            <h3 className='md:text-2xl text-xl font-bold mb-7 text-gray-800 dark:text-white'>
               {tabs.find((t) => t.id === activeTab)?.name}
             </h3>
 
-            <div className='prose dark:prose-invert grid grid-cols-5 gap-4'>
+            <div className='prose dark:prose-invert grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4'>
               {tabs.map((tab) => {
-                return tab.content.map((contentItem) => {
+                if(activeTab === tab.id){
+                  return tab.content && tab.content.map((contentItem) => {
                   return <Badge>
                     <div className='flex items-center gap-2 py-1 px-2'>
-                      <span>{contentItem.icon}</span>
-                      <span className='font-bold text-lg'>{contentItem.name}</span>
+                      <span>{contentItem?.icon}</span>
+                      <span className='font-bold text-md md:text-lg'>{contentItem?.name}</span>
                     </div>
                   </Badge>
                 })
+                }
               })}
             </div>
 
-            <div className='mt-6 text-center'>
+            {/* <div className='mt-6 text-center'>
               <button
                 className={`px-4 py-2 rounded-full text-white bg-gradient-to-r ${tabs.find((t) => t.id === activeTab)?.color}`}
               >
                 Explore {tabs.find((t) => t.id === activeTab)?.name}
               </button>
-            </div>
+            </div> */}
           </motion.div>
         </AnimatePresence>
       </div>
